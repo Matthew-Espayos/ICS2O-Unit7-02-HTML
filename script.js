d5b4ab7aad36c8de4d34bb35feb62c0f2fb0eab4
MatthewEspayos <>
2021-06-08 14:46:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Variables to store the users input
+let userinput = 0
+// Connect "Positive?" button to "random" function
+document.getElementById('mybutton').addEventListener('click', comparison)
+// Get user input and convert it to an integer
+function comparison () {
+  userinput = document.getElementById('myinput').value
+  userinput = parseInt(userinput)
+// Check if the user input is less than 0 (negative)
+  if (userinput < 0) {
+    document.getElementById('myanswer').innerHTML = 'Negative'
+  } else {
+    document.getElementById('myanswer').innerHTML = 'Positive'
+  }
+}
